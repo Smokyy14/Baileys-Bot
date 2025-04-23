ef80d0b87bea2816a27528f43faa107fbf6af0ff
Smokyy14 <fdsmdfr985@gmail.com>
2025-04-23 14:37:21 -0300

    Update menu.js

--- src/commands/General/menu.js
+++ src/commands/General/menu.js
@@ -1,20 +1,20 @@
 module.exports = {
   name: "menu",
   alias: ["m"], 
-  category: "Comandos",
+  category: "General",
   use: "/menu",
-  description: "Despliega la lista de comandos.",
+  description: "Shows the command menu.",
     
   async execute(sock, msg, _) {
-    const commands = sock.commands.filter(({ dev }) => !dev ); // Filtra los comandos con el campo "dev: true,"
+    const commands = sock.commands.filter(({ dev }) => !dev ); // Filters commands with the "dev: true" field
     const jid = msg.messages[0].key.remoteJid;
 
     if (!commands.size) {
-      return msg.reply("Sin comandos para mostrar.");
+      return msg.reply("No commands to show.");
     }
 
       const categorized = commands.reduce((previous, current) => {
-      const category = current.category || "Sin categoría";
+      const category = current.category || "Without category";
       const subcategory = current.subcategory || null;
 
       if (!previous[category]) {
@@ -27,23 +27,23 @@ module.exports = {
         }
         previous[category][subcategory].push(current);
       } else {
-        if (!previous[category]["Sin subcategoría"]) {
-          previous[category]["Sin subcategoría"] = [];
+        if (!previous[category]["Without subcategory"]) {
+          previous[category]["Without subcategory"] = [];
         }
-        previous[category]["Sin subcategoría"].push(current);
+        previous[category]["Without subcategory"].push(current);
       }
 
       return previous;
     }, {});
 
-    let text = `Tengo *${commands.size}* comandos para tu uso:\n`;
+    let text = `I have *${commands.size}* commands to you:\n`;
 
     for (const [category, subcategories] of Object.entries(categorized)) {
       text += `\n> ------------------------\n`;
       text += `\n*${category}:*\n`;
 
       for (const [subcategory, commands] of Object.entries(subcategories)) {
-        if (subcategory !== "Sin subcategoría") {
+        if (subcategory !== "Without subcategory") {
           text += `      *${subcategory}*:\n`;
         }
         const list = commands.map(({ name }) => `\t\t\`/${name}\``).join("\n");
