029645b4f90a01fe270d519ad7959bf143e7ab73
Smokyy14 <fdsmdfr985@gmail.com>
2025-09-03 23:09:49 -0300

    Update messages.upsert.js

--- src/events/messages.upsert.js
+++ src/events/messages.upsert.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+const { getGroupAdmins } = require("../utils/getAdmins.js");
+const { jidNormalizedUser } = require("baileys");
+
 module.exports = {
   name: "messages.upsert",
 
@@ -5,7 +10,7 @@ module.exports = {
     if (msg.type !== "notify") return;
     const { message: ctx, key } = msg.messages[0];
     require("../utils/messageUtils")(sock, msg);
-      
+
     const btn = ctx?.templateButtonReplyMessage || ctx?.buttonsResponseMessage;
     if (btn && typeof btn === "object") {
       const selected = btn?.selectedId || btn?.selectedButtonId;
@@ -17,8 +22,15 @@ module.exports = {
       return component.execute(sock, msg, args);
     }
 
-    let userId = key.remoteJid;
+    const chatId = key.remoteJid;
+    const senderId = key.participant || key.remoteJid;
 
+    msg.from = chatId;
+    msg.sender = senderId;
+    msg.isGroup = chatId.endsWith("@g.us");
+    msg.isOwner = ["yourNumber@s.whatsapp.net"].includes(senderId);
+    const jidToCheck = msg.isGroup ? jidNormalizedUser(msg.sender) : key.remoteJid;
+  
     const body =
       ctx?.extendedTextMessage?.text ||
       ctx?.ephemeralMessage?.message?.extendedTextMessage?.text ||
@@ -26,11 +38,7 @@ module.exports = {
       ctx?.imageMessage?.caption ||
       ctx?.videoMessage?.caption;
 
-    if (body === "prefix") {
-      return msg.reply("_My prefix is:_ `/`");
-    }
-
-    if (!body || !["/"].some((prefix) => body.startsWith(prefix)) || body.length <= 1) return;
+    if (!body || !["!"].some((prefix) => body.startsWith(prefix)) || body.length <= 1) return;
 
     const args = body.slice(1).trim().split(" ");
     const label = args.shift().toLowerCase();
@@ -40,31 +48,30 @@ module.exports = {
     );
 
     if (!command) {
-      return msg.reply("```This command no exists.```");
-    }
-      
-    if (command.dev && !sock.config.dev.includes(userId)) {
-      return msg.reply("```This command is only available to dev users.```");
+      return msg.reply(`El comando ${label} no existe.\nRevisa la lista de comandos con:\n-menu.`);
     }
 
-    if (key.remoteJid.includes("@g.us") && command.admin) {
-      const grupoMetadata = await sock.groupMetadata(key.remoteJid);
-      const grupoAdmins = grupoMetadata.participants
-        .filter((participant) => participant.admin === "admin" || participant.admin === "superadmin")
-        .map((admin) => admin.id);
+    let isGroupAdmin = false;
+    let isBotAdmin = false;
 
-      if (!grupoAdmins.includes(key.participant)) {
-        return sock.sendMessage(key.remoteJid, {
-          text: "This command is only available to the admins of the group",
-          quoted: msg.messages[0],
-        });
-      }
+    if (msg.isGroup) {
+      const groupMetadata = await sock.groupMetadata(chatId);
+      const groupParticipants = groupMetadata.participants || [];
+      const groupAdmins = getGroupAdmins(groupParticipants);
+
+      isGroupAdmin = groupAdmins.includes(jidNormalizedUser(senderId));
+      isBotAdmin = groupAdmins.includes(jidNormalizedUser(sock.user.lid));
     }
+
+    if (command?.premium && !isPremium) return msg.reply("Este comando es exclusivo para usuarios premium.");
+    if (command?.group && !msg.isGroup) return msg.reply("Este comando solo se puede utilizar en grupos.");
+    if (command?.priv && msg.isGroup) return msg.reply("Este comando solo se puede utilizar en privado.");
+    if (command?.admin && msg.isGroup && !isGroupAdmin && !msg.isOwner) return msg.reply("Este comando es solo para administradores.");
+    if (command?.botadmin && msg.isGroup && !isBotAdmin) return msg.reply("El bot necesita ser administrador para ejecutar este comando.");
 
     try {
-      await command.execute(sock, msg, args);
-    } catch (error) {
-      msg.reply("Something went wrong while running the command").then(() => {
+      await command.execute(sock, msg, args);} catch (error) {
+      msg.reply("Ocurrió un error al ejecutar el comando.").then(() => {
         console.error(error);
       });
     }
